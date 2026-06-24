5a9d2c1c0de6c4bd40c2cf80e816d8aa637d18ea
itpofy2024o <qweasdzxcf1G@proton.me>
2026-06-25 01:37:46 +0800

    a

--- reset-admin.js
+++
@@ -1,9 +0,0 @@
-import Database from 'better-sqlite3';
-import { createHash } from 'crypto';
-
-const db = new Database('orega.db');
-const hash = createHash('sha256').update('osossosohaha').digest('hex');
-
-db.prepare('UPDATE admin_config SET value = ? WHERE key = ?').run(hash, 'admin_password');
-console.log('Admin password reset to: osossosohaha');
-db.close();
--- test-sqlite.js
+++
@@ -1,7 +0,0 @@
-const Database = require('better-sqlite3');
-try {
-    const db = new Database(':memory:');
-    console.log('Successfully loaded better-sqlite3');
-} catch (e) {
-    console.error('Failed to load better-sqlite3:', e);
-}
